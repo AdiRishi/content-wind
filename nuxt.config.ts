9a86149a735cfb5a0c7245627a7c94dff110d964
Adishwar Rishi <adiswa123@gmail.com>
2023-11-05 18:32:39 +1100

    Add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,9 @@
 export default defineNuxtConfig({
-  extends: 'content-wind'
-})
+  extends: "content-wind",
+  runtimeConfig: {
+    public: {
+      studioTokens:
+        "6a0b8c9e70051717f29ea416a03a3003f9d446186376a93c0d671834a318c824",
+    },
+  },
+});
